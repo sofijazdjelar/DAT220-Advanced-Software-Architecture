d27e951ebd007d95247f9a255ec4d4b1d6d15493
Konstantin Ay <konstantinayold@Konstantins-MacBook-Pro.local>
2020-02-26 11:54:46 +0100

    Added locking mechanisms

--- windowLockMechanism.py
+++ windowLockMechanism.py
@@ -2,13 +2,14 @@ import paho.mqtt.client as mqtt
 
 def on_message(client, userdata, message):
     print("Message Recieved: "+message.payload.decode())
+    # Publishing to datatransmitter
+    client.publish("dt/window_lock", message.payload.decode())
+
 
 client = mqtt.Client()
 client.on_message = on_message
 client.connect("127.0.0.1", 1883, 60)
 
-client.subscribe("dt/smoke", qos=1)
-client.subscribe("dt/movement", qos=1)
-client.subscribe("dt/locks", qos=1)
+client.subscribe("security/window_lock", qos=1)  # Waiting on mock event
 
 client.loop_forever()
