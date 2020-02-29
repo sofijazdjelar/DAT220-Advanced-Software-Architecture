f048b651b5e7b23749dd39f40334680e4f78412f
Konstantin Ay <konstantinayold@Konstantins-MacBook-Pro.local>
2020-02-29 22:11:24 +0100

    Added Firebase realtime db integration

--- dataTransmitter.py
+++ dataTransmitter.py
@@ -1,7 +1,31 @@
 import paho.mqtt.client as mqtt
+import time
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('key.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://ambient-assistant-living.firebaseio.com/'
+})
+
+# TODO: Add conditions based on the topic
+
 
 def on_message(client, userdata, message):
-    print("Data Transmitter Recieved: "+message.payload.decode())
+    topic = message.topic
+
+    if topic in ('dt/door_lock', 'dt/window_lock'):
+        m_decode = str(message.payload.decode("utf-8", "ignore"))
+        m_in = json.loads(m_decode)
+        ref = db.reference(topic)
+        child = ref.child(m_in["id"])
+        time.sleep(1)
+        child.update(m_in)
+
 
 client = mqtt.Client()
 client.on_message = on_message
@@ -9,7 +33,8 @@ client.connect("127.0.0.1", 1883, 60)
 
 client.subscribe("dt/smoke", qos=1)
 client.subscribe("dt/movement", qos=1)
-client.subscribe("dt/locks", qos=1)
+client.subscribe("dt/door_lock", qos=1)
+client.subscribe("dt/window_lock", qos=1)
 client.subscribe("dt/heart_rate", qos=1)
 client.subscribe("dt/step_count", qos=1)
 client.subscribe("dt/glucose_level", qos=1)
--- mockEvents.py
+++ mockEvents.py
@@ -1,14 +1,27 @@
 import paho.mqtt.client as mqtt
 import time
+import json
 
 client = mqtt.Client()
 client.connect("127.0.0.1", 1883, 60)
 client.publish("safety/smoke", "Mock Event: Smoke Alarm Triggered")
-time.sleep(10)
+# # time.sleep(10)
 client.publish("safety/movement", "Mock Event: Movement Detected")
-time.sleep(10)
-client.publish("security/window_lock", "Mock Event: Window Opened")
-time.sleep(10)
-client.publish("security/door_lock", "Mock Event: Door Opened")
-time.sleep(10)
-client.disconnect() 
+# time.sleep(10)
+client.publish("security/window_lock",
+               json.dumps({'id': '1', 'isOpen': 'false'}))
+client.publish("security/window_lock",
+               json.dumps({'id': '2', 'isOpen': 'false'}))
+client.publish("security/window_lock",
+               json.dumps({'id': '3', 'isOpen': 'true'}))
+client.publish("security/window_lock",
+               json.dumps({'id': '4', 'isOpen': 'true'}))
+client.publish("security/window_lock",
+               json.dumps({'id': '5', 'isOpen': 'false'}))
+# time.sleep(10)
+client.publish("security/door_lock",
+               json.dumps({'id': '1', 'isOpen': 'false'}))
+client.publish("security/door_lock",
+               json.dumps({'id': '2', 'isOpen': 'false'}))
+# time.sleep(10)
+client.disconnect()
