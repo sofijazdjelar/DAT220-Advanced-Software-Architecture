9f849db9e2d043eff2d1cc9dc14614ab4b309aa1
Konstantin Ay <konstantinayold@Konstantins-MacBook-Pro.local>
2020-02-29 22:11:35 +0100

    Changed to json formatting of publish payload

--- mockEvents.py
+++ mockEvents.py
@@ -5,23 +5,28 @@ import json
 client = mqtt.Client()
 client.connect("127.0.0.1", 1883, 60)
 client.publish("safety/smoke", "Mock Event: Smoke Alarm Triggered")
-# # time.sleep(10)
+time.sleep(10)
 client.publish("safety/movement", "Mock Event: Movement Detected")
-# time.sleep(10)
+time.sleep(10)
 client.publish("security/window_lock",
                json.dumps({'id': '1', 'isOpen': 'false'}))
+time.sleep(10)
 client.publish("security/window_lock",
                json.dumps({'id': '2', 'isOpen': 'false'}))
+time.sleep(10)
 client.publish("security/window_lock",
                json.dumps({'id': '3', 'isOpen': 'true'}))
+time.sleep(10)
 client.publish("security/window_lock",
                json.dumps({'id': '4', 'isOpen': 'true'}))
+time.sleep(10)
 client.publish("security/window_lock",
                json.dumps({'id': '5', 'isOpen': 'false'}))
-# time.sleep(10)
+time.sleep(10)
 client.publish("security/door_lock",
                json.dumps({'id': '1', 'isOpen': 'false'}))
+time.sleep(10)
 client.publish("security/door_lock",
                json.dumps({'id': '2', 'isOpen': 'false'}))
-# time.sleep(10)
+time.sleep(10)
 client.disconnect()
